45b36b32b7180f45598556ab2f3c8bd5c8f89af1
dtrihinas <trihinas@cs.ucy.ac.cy>
2013-06-17 11:59:53 +0300

    first commit - Probe Package

---
+++ ms/src/eu/celarcloud/celar_ms/ProbePack/ddd.java
@@ -0,0 +1,13 @@
+package eu.celarcloud.celar_ms.ProbePack;
+
+public class ddd {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
